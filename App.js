4cfe7b1e86ed6f962b361c3f06b7cb1f5b27ffd4
alannmartinezz@icloud.com <alannmartinezz@icloud.com>
2023-04-22 06:07:01 -0300

    This is my first commit learning React Native

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const name = "Alan";
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello {name}, how are you?</Text>
+      <View style={styles.flexContainer}>
+        <TextInput style={styles.input} placeholder="Type here..."></TextInput>
+        <Button title="Add to cart" />
+      </View>
     </View>
   );
 }
@@ -13,8 +17,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  flexContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  input: {
+    borderBottomColor: "cyan",
+    borderBottomWidth: 1,
+  }
 });
